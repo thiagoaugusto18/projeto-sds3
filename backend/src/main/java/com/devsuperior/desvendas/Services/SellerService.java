9237b0775a13a9544578dec4760e7a8ffbe3ef27
Thiago Costa <thiagoaugusto_18@yahoo.com.br>
2021-11-03 21:13:30 -0300

    layers

---
+++ backend/src/main/java/com/devsuperior/desvendas/Services/SellerService.java
@@ -0,0 +1,25 @@
+package com.devsuperior.desvendas.Services;
+
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.devsuperior.desvendas.dto.SellerDTO;
+import com.devsuperior.desvendas.entities.Seller;
+import com.devsuperior.desvendas.repositories.SellerRepository;
+
+@Service
+public class SellerService {
+	
+	@Autowired
+	private SellerRepository repository;
+	
+	public List<SellerDTO> findAll(){
+		List<Seller> result = repository.findAll();	
+		return result.stream().map(x -> new SellerDTO(x)).collect(Collectors.toList());
+		
+	}
+}
